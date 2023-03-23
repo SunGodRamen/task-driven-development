bfc3332a04923cd7340eadc39a1e3e77775171b6
AVONSTU1 <AVONSTU1@ford.com>
2023-03-22 22:14:08 -0400

    kt git function source option

--- .git_hooks/_run_hooks.sh
+++ .git_hooks/_run_hooks.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env  bash
 
-# Add this to your shell configuration (e.g., .bashrc or .zshrc)
+# Source this in your shell configuration as an option instead of moving the hooks to the project .git/ dir
+# if you rapid prototype, this can save time each time you copy the project as .git cant be tracked
 function gitp() {
     GIT_DIR=$(git rev-parse --git-dir)
     git --git-dir="$GIT_DIR" --work-tree="$(pwd)" --exec-path="$GIT_DIR/../.git_hooks" "$@"
